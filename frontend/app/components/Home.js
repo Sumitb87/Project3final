5fd7390f94b05360344b0dbd444db143060549f7
Tanya Newell <trozova5@gmail.com>
2016-04-12 16:53:00 -0400

    updated frontend

---
+++ frontend/app/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router';
+import Search from './Search';
+import Filters from './Filters';
+import DisplayResults from '.DisplayResults';
+
+const Home = React.createClass({
+  clickConfirm: function(){
+    console.log('button was clicked!');
+  },
+  render: function(){
+    return(
+      <div>
+        <h1>Brunch Of Places</h1>
+        <Search />
+        <Filters />
+        <DisplayResults />
+        <Link to="/addRestaurant"><button onClick={this.clickConfirm}>Add Restaurant</button></Link>
+        <br/><br/>
+
+      </div>
+    )
+  }
+});
+
+export default Home;
